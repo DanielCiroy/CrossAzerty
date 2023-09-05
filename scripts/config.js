4bae890a71555f18aa94557db7cc31f3420c4a1f
Daniel Ciroy <danielciroy87@gmail.com>
2023-09-06 01:16:29 +0200

    envoi du jeu cossazerty sur github

---
+++ scripts/config.js
@@ -0,0 +1,11 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette","Manchette","Corbeil","Safari","Bouillon","Graine","Bourgeon","cigarette"]
+
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson","Bonjour monsieur","Je pense a mon avenir","Mon amie le lui offre","Le professeur est absent aujourd'hui", "Cette roue tourne vite!","la fete est prevue pour ce soir","Dieu est grand"]
